bd63589338a4cb05b43ce278bd4dcff02f078234
John Craven <johnrobertcraven@gmail.com>
2016-09-07 22:09:42 -0600

    Identify LandSat WRS2 path/row fnc

--- get_landsat.py
+++ get_landsat.py
@@ -1,6 +1,8 @@
 import urllib
 import os
-
+import pandas as pd
+from geopy.distance import vincenty
+import numpy as np
 
 """
 Purpose: automate download of LandSat data from Amazon S3 Bucket
@@ -35,6 +37,35 @@ def build_url(sat='LC',sens='8',spath='003', srow='017',yr='2014',
 	scene_url = 'https://s3-us-west-2.amazonaws.com/landsat-pds/'+'L'+sens+'/'+spath+'/'+srow+'/'+scene_end+'/'
 
 	return scene_img, scene_url
+
+def latlong_to_wrs2(lat_in, long_in):
+	
+	tmp_site = (lat_in, long_in)
+	
+	#scene_list.csv contains path/row and location data for all scenes 
+	locs = pd.read_csv('scene_list.csv')
+	
+	dist = np.zeros([locs.shape[0]])
+	
+	#Calculate distance from site to all scene centroids in miles
+	for i in range(0,locs.shape[0]):
+		scene_cent = (locs['lat_cent'].values[i],locs['long_cent'].values[i])
+		dist[i] = vincenty(tmp_site, scene_cent).miles
+	
+	#get index - e.g. closest centroid to site
+	idx = np.argmin(dist)
+	wrs_path = locs['Path'].values[idx]
+	wrs_row = locs['Row'].values[idx]
+	
+	#Check to get Day-time scene
+	if lat_in > 0.:
+		while wrs_path > 50:
+			dist = np.delete(dist,idx)
+			idx = np.argmin(dist)
+			wrs_path = locs['Path'].values[idx]
+			wrs_row = locs['Row'].values[idx]
+		
+	return wrs_path, wrs_row
 #TO DO
 """
 Get scene list from AWS
@@ -42,11 +73,19 @@ Get scene list from AWS
 -Prompt selection of scenes
 -Download to folder
 """
+#Near San Francisco
+my_lat = 37.7
+my_long = -122.5
+
+my_path, my_row = latlong_to_wrs2(my_lat,my_long)
+
+print my_path
+print my_row 
 
 #
-tmp_imgname, tmp_url  = build_url()
+#tmp_imgname, tmp_url  = build_url()
 
 #Get image from Amazon S3 bucket
-urllib.urlretrieve(tmp_url+tmp_imgname, tmp_imgname)
+#urllib.urlretrieve(tmp_url+tmp_imgname, tmp_imgname)
 
 
